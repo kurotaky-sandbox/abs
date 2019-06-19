788bfd2877d0d0aa1bc6f2e7e64d6ee39c956f4a
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2019-06-20 01:07:38 +0900

    Shift only

--- Shift_only/a.rb
+++ Shift_only/a.rb
@@ -1,3 +1,15 @@
-n = gets
+n = gets.chomp.to_i
 numbers = gets.chomp.split(' ').map(&:to_i)
 
+count = 0
+
+loop do
+  if numbers.map{|ar| ar.even?}.all?
+    numbers.map!{|ar| ar / 2}
+    count = count + 1
+  else
+    break
+  end
+end
+
+puts count
