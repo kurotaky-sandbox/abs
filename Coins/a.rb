55e680685798d8c1feb1d8b5b9adbcd1a198bfdb
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2019-06-26 08:51:22 +0900

    WIP

--- Coins/a.rb
+++ Coins/a.rb
@@ -3,4 +3,19 @@ b = gets.chomp.to_i # 100円
 c = gets.chomp.to_i # 50円
 x = gets.chomp.to_i # 合計金額
 
+count = 0
 
+a_arr = [500] * a
+b_arr = [100] * b
+c_arr = [50] * c
+
+a_arr.each_with_index do |va, i|
+  b_arr.each_with_index do |vb, j|
+    c_arr.each_with_index do |vc, k|
+      p count
+      count = count + 1 if (va * i + vb * j + vc * k) == x
+    end
+  end
+end
+
+puts count
