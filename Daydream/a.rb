ab2ebabfc08dcd69fbed49f3b008d05abf0a2e41
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2019-07-30 00:23:46 +0900

    シンプルな解にたどり着いた

--- Daydream/a.rb
+++ Daydream/a.rb
@@ -2,18 +2,10 @@
 
 s = gets.chomp
 
-2000.times do
-  s.slice!(0, 6) if s.start_with?('eraser')
-  s.slice!(0, 5) if s.start_with?('erase') && !s.start_with?('eraser')
-  s.slice!(0, 5) if s.start_with?('dreameraser') || s.start_with?('dreamerased') || s.start_with?('dreamerasee')
-  s.slice!(0, 5) if s.start_with?('dreamerase') && !s.start_with?('dreameraser')
-  s.slice!(0, 7) if s.start_with?('dreamer') && s[7..9] != 'ase'
-  s.slice!(0, 5) if s.start_with?('dream') && (s[5..7] != 'erd' || s[5..7] != 'ere')
-  break if s.size == 0
-end
+ans = s.gsub('eraser', '').gsub('erase', '').gsub('dreamer', '').gsub('dream', '')
 
-if s.size > 0
-  puts 'NO'
-else
+if ans.empty?
   puts 'YES'
+else
+  puts 'NO'
 end
